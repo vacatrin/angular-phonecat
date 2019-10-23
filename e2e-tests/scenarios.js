9bcbfeb89dbcb36124584555b49900e1edc71629
Victor Acatrinei <Victor.Acatrinei@endava.com>
2019-10-23 11:03:56 +0300

    Completed step 2 exercises.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -8,5 +8,13 @@ describe('My app', function() {
   beforeEach(function() {
     browser.get('index.html');
   });
+});
 
+describe('PhoneListCtrl', function(){
+    it('should create "phones" model with 3 phones', function() {
+        var scope = {},
+            ctrl = new PhoneListCtrl(scope);
+
+        expect(scope.phones.length).toBe(4); 
+    })
 });
--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
 
     plugins: [
       'karma-chrome-launcher',
